e6a5e7002619edff8233a0893ee5466b0934c613
Jack Bodine <JackTaylorBodine@gmail.com>
2020-03-11 22:12:15 -0700

    Added "Boss Problems," cleaned code and added comments.
    -Boss problems spawn randomly after the first ten problems. They are more difficult by having higher values.
    
    -Added comments to game.php
    
    -Cleaned up the code A LOT and deleted unessesary parts.

--- Project Folder/game.php
+++ Project Folder/game.php
@@ -1,200 +1,237 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<link rel="stylesheet" type="text/css"href="stylesheet.css">
-<style>
-canvas {
-    border:1px solid #d3d3d3;
-    background-color: #000000;
-}
-</style>
+    <title>Matheroids</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" type="text/css"href="stylesheet.css">
+    <style>
+        canvas {border:1px solid #a9a9a9; background-color: #000000}
+    </style>
 </head>
+
 <body onload="startGame()">
 
-<?php
-
-   function prepare_query_string(){
-        $re = [];
-        $query_array = explode("&", $_SERVER["QUERY_STRING"]);
-        foreach ($query_array as $key => $value) {
-        $temp = explode("=", $value);
-        $re[$temp[0]] = $temp[1]; 
+    <?php
+        function prepare_query_string(){
+            $re = [];
+            $query_array = explode("&", $_SERVER["QUERY_STRING"]);
+            foreach ($query_array as $key => $value) {
+            $temp = explode("=", $value);
+            $re[$temp[0]] = $temp[1]; 
+            }
+            return $re;
         }
-        return $re;
-    }
-?>
+    ?>
 
-<script type="text/javascript"> 
-    function createAdditionProblem() {
-            var min=1; 
-            var max=10;  
-            var num1 = getRandomNumber(min, max)
-            var num2 = getRandomNumber(min, max)
-            var ans = num1+num2; 
+    <br />
+
+    <script type="text/javascript">
+
+        //Generates a random addition problem between min and max. Returns the problem in string form.
+        function createAdditionProblem(min, max) { 
+            var num1 = getRandomNumber(min, max);
+            var num2 = getRandomNumber(min, max);
             var problem = num1 +"+"+num2 +"=";
+
             return problem;
         }
-        createAdditionProblem();
+            
+        //Solves an addition problem in the form "a+b=". Returns the answer.
+        function solveAdditionProblem(problem){
+            var operatorLocation;
+            var equalLocation;
 
-    function solveAdditionProblem(problem){
-        var num1 = parseInt(problem.slice(0,1));
-        var num2 = parseInt(problem.slice(2,3));
-        var ans = num1+num2;
-        console.log(num1+" + "+num2+"="+ans);
-        return ans;
-    }
-
-    function getRandomNumber(min, max){
-        var num = Math.random() * (+max - +min) + +min; 
-        num = Math.floor(num)
-        return num;
-    }
-
-</script> 
-<br />
-
-
-<script>
-
-var myGamePieces = [];
-var playing = true;
-var score = 0;
-
-var spawnMax = 300;
-var spawnTimer = 200; 
-
-function startGame() {
-    addPiece();
-    myGameArea.start();
-}
-
-function addPiece() {
-    var xSpawn = getRandomNumber(10,300); 
-
-    var myGamePiece = new component(30, 30, "white", xSpawn, -50);
-    myGamePieces.push(myGamePiece);
-}
-
-var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 400;
-        this.canvas.height = 600;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 15);        
-    },
-    stop : function() {
-        clearInterval(this.interval);
-    },    
-    clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-}
-
-function component(width, height, color, x, y, type) {
-    this.problem = createAdditionProblem();
-    this.answer = solveAdditionProblem(this.problem);
-    //this.problem = color;
-    //console.log(mathProblem)
-    this.type = type;
-    this.color = color;
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = y;    
-    this.speedY = 0;    
-    this.gravitySpeed = 1;
-    this.update = function() {
-        ctx = myGameArea.context;
-        ctx.fillStyle = this.color;
-        ctx.font = "16px Courier New";
-        ctx.fillText(this.problem,this.x,this.y);
-    }
-    this.newPos = function() {
-        this.gravitySpeed += 0.00;
-        this.y += this.speedY + this.gravitySpeed;    
-        this.hitBottom();    
-    }
-    this.hitBottom = function() {
-        var rockbottom = myGameArea.canvas.height - this.height;
-        if (this.y > rockbottom) {
-            youLose();
-        }
-    }
-    this.getAnswer = function(){
-        return this.answer;
-    }
-    this.updateColor = function(newColor){
-        this.color = newColor;
-    }
-}
-
-function updateGameArea() {
-    if (playing == true){
-        myGameArea.clear();
-        for(var i = 0; i < myGamePieces.length; i++){
-        myGamePieces[i].newPos();
-        myGamePieces[i].update();
-        }
-        spawnTimer--;
-        if(spawnTimer <=0){
-            spawnTimer = getRandomNumber(10,spawnMax);
-            addPiece();
-        }
-        myGamePieces[0].updateColor('Aqua');
-    }
-}
-
-function increaseScore(){
-
-    document.getElementById("score").innerHTML = "Score: "+score;
-
-}
-
-function youLose(){
-    document.getElementById("score").innerHTML = "You Lose! Failure! You utter baffoon! You are not smart, moron! You will never accomplish anything. Santa Claus isn't real and neither is happiness.\n Your final score was: "+score;
-
-    playing = false;
-}
-
-function checkAnswer(){
-        if(playing == true){
-    var ans = document.getElementById("userAnswer").value;
-    console.log("USER ANSWER: "+ans);
-
-    for(var i = 0; i < myGamePieces.length; i++){
-        if(ans == myGamePieces[i].getAnswer()){
-            myGamePieces.splice(i, i+1);
-            score++;
-            if (spawnMax >= 80){
-                spawnMax = spawnMax - getRandomNumber(0,20);
+            for(var i = 0; i < problem.length; i++){
+                if(problem.charAt(i) == "+"){
+                    operatorLocation = i;
+                } else if (problem.charAt(i) == "="){
+                    equalLocation = i;
+                }
             }
-            increaseScore();
 
+            var num1 = parseInt(problem.slice(0,operatorLocation));
+            var num2 = parseInt(problem.slice(operatorLocation+1,equalLocation));
+            var ans = num1+num2;
+            console.log(num1+" + "+num2+"="+ans);
+
+            return ans;
         }
-    }
-    document.getElementById("userAnswer").value = "";
-}
-}
 
-</script>
+        //Generates a random numbers between a given a min/max. Returns the number.
+        function getRandomNumber(min, max){
+            var num = Math.random() * (+max - +min) + +min; 
+            num = Math.floor(num)
 
-<p id="score">Score: 0</p>
+            return num;
+        }
 
-<br />
+        var problems = [];
+        var playing = true;
+        var score = 0;
+
+        var spawnMax = 300;
+        var spawnTimer = 200;
+        var bossCountdown = 10;
+
+        function startGame() {
+            addProblem();
+            gameArena.start();
+        }
+
+        //Adds a problem to the array of problems.
+        function addProblem() {
+            var xSpawn = getRandomNumber(10,300); 
+
+            var myGamePiece = new object(xSpawn, -50);
+            problems.push(myGamePiece);
+            bossCountdown--;
+        }
+
+        var gameArena = {
+            canvas : document.createElement("canvas"),
+            start : function() {
+                this.canvas.width = 400;
+                this.canvas.height = 600;
+                this.context = this.canvas.getContext("2d");
+                document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+                this.interval = setInterval(updateGameArea, 15);        
+            },
+            clear : function() {
+                this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            }
+        }
+
+        function object(x, y) {
+            this.problem = createAdditionProblem(1, 10);
+
+            this.x = x;
+            this.y = y;
+            this.speed = 1;
+            this.color = 'White';
+            this.boss = false;
+
+            //Checks if it's time to create a boss problem.
+            if(bossCountdown == 0){
+                this.problem = createAdditionProblem(5,20);
+                this.color = "Yellow";
+                this.boss = true;
+                bossCountdown = getRandomNumber(5,15);
+            }
+
+            this.answer = solveAdditionProblem(this.problem);
+
+            //Deletes and redraws the equation at its new location.
+            this.update = function() {
+                ctx = gameArena.context;
+                ctx.fillStyle = this.color;
+                ctx.font = "16px Courier New";
+                ctx.fillText(this.problem,this.x,this.y);
+            }
+
+            //Finds the new location for an equation
+            this.newPosition = function() {
+                this.y += this.speed;    
+                this.hitBottom();    
+            }
+
+            //Checks to see if the equation has hit the bottom.
+            this.hitBottom = function() {
+                var bottom = gameArena.canvas.height - 18;
+                if (this.y > bottom) {
+                    youLose();
+                }
+            }
+
+            //Returns the answer of the equation.
+            this.getAnswer = function(){
+                return this.answer;
+            }
+
+            //Updates the color of an equation
+            this.updateColor = function(newColor){
+                this.color = newColor;
+            }
+
+            //Returns wether or not the problem is a boss.
+            this.getBossStatus = function(){
+                return this.boss;
+            }
+        }
+
+        //Reutns every tick.
+        function updateGameArea() {
+            if (playing == true){
+
+                gameArena.clear();
+                spawnTimer--;
+
+                for(var i = 0; i < problems.length; i++){
+                problems[i].newPosition();
+                problems[i].update();
+                }
+
+                if(spawnTimer <= 0){
+                    spawnTimer = getRandomNumber(10,spawnMax);
+                    addProblem();
+                }
+
+                if((problems.length >= 1)&&(problems[0].getBossStatus() == false)){
+                    problems[0].updateColor('Aqua');
+                }else{
+                    problems[0].updateColor('Orange');
+                }
+            }
+        }
+
+        //Updates the printed Score
+        function updateScore(){
+            document.getElementById("score").innerHTML = "Score: "+score;
+        }
+
+        //Tells the player they lose
+        function youLose(){
+            playing = false;
+            document.getElementById("score").innerHTML = "You Lose! Failure! You utter baffoon! You are not smart, moron! You will never accomplish anything. Santa Claus isn't real and neither is happiness.\n Your final score was: "+score;
+        }
+
+        //Checks the users given answer.
+        function checkAnswer(){
+            if(playing == true){
+                var ans = document.getElementById("userAnswer").value;
+
+                for(var i = 0; i < problems.length; i++){
+
+                    if(ans == problems[i].getAnswer()){
+                        problems.splice(i, i+1);
+                        score++;
+                        updateScore();
+
+                        if (spawnMax >= 80){
+                            spawnMax = spawnMax - getRandomNumber(0,20);
+                        }
+
+                        break;
+                    }
+                }
+                document.getElementById("userAnswer").value = "";
+            }
+        }
+
+    </script>
+
+    <p id="score">Score: 0</p>
+
     <form onsubmit="checkAnswer(); return false;" autocomplete="off">
         Answer:
         <input type="text" name="answer"  id="userAnswer" placeholder="answer" autofocus/>
-        <br/>
-        <br/>
     </form>
 
-    <form action="../mainMenu.php/?user=$user">
-        <button type="submit">Return to main menu</button></a>
-    </form>
+    <br />
 
+    <form action="../mainMenu.php/?user=$user" >
+        <button type="submit">Return to main menu</button>
+    </form>
 
 </body>
 </html>
