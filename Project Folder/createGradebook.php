483979f7d2f27ce0040b0206f2320ca98a7b9120
Jack Bodine <JackTaylorBodine@gmail.com>
2020-05-08 20:24:55 -0500

    Gradebook doesn't include teachers. Fixed bug with adding students.

--- Project Folder/createGradebook.php
+++ Project Folder/createGradebook.php
@@ -6,32 +6,34 @@
 
 	$all_user = get_user_info(USERFILE);
 
-	foreach ($all_user as $user) { 					
-		echo "<tr>";
-		for ($i = 1; $i <= 6; $i+=1){
-			echo "<td>";
-				if($i == 1){
-					$firstName = $user["first"];
-					echo "$firstName";
-				}else if($i == 2){
-					$lastName = $user["last"];
-					echo "$lastName";
-				}else if($i == 3){
-					$level1score = $user["level1Score"];
-					echo "$level1score";
-				}else if($i == 4){
-					$level2score = $user["level2Score"];
-					echo "$level2score";
-				}else if($i == 5){
-					$level3score = $user["level3Score"];
-					echo "$level3score";
-				}else if($i == 6){
-					$level4score = $user["level4Score"];
-					echo "$level4score";
-				}
-			echo "</td>";
+	foreach ($all_user as $user) { 	
+		if($user["class"] == 'student'){			#Doesn't include teachers, eventually will only include students in a class.				
+			echo "<tr>";
+			for ($i = 1; $i <= 6; $i+=1){
+					echo "<td>";
+					if($i == 1){						#I am not entirely sure if I can just do echo "$user[$i]" so there are a bunch of if statements.
+						$firstName = $user["first"];
+						echo "$firstName";
+					}else if($i == 2){
+						$lastName = $user["last"];
+						echo "$lastName";
+					}else if($i == 3){
+						$level1score = $user["level1Score"];
+						echo "$level1score";
+					}else if($i == 4){
+						$level2score = $user["level2Score"];
+						echo "$level2score";
+					}else if($i == 5){
+						$level3score = $user["level3Score"];
+						echo "$level3score";
+					}else if($i == 6){
+						$level4score = $user["level4Score"];
+						echo "$level4score";
+					}
+				echo "</td>";			
+			}
+			echo "</tr>";
 		}
-		echo "</tr>";
 	} 
 
 ?>
