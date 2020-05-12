5f6170a6d33d53fd01bb0db59628e7c9a500539e
apc518 <56745633+apc518@users.noreply.github.com>
2020-05-12 14:17:23 -0700

    more login improvements
    made the login forms a bit bigger, credited the artist in stylesheet.css instead of welcome.php

--- Project Folder/welcome.php
+++ Project Folder/welcome.php
@@ -12,22 +12,20 @@
 </head>
 
 <body class="welcomeBody">
-	<!--background image free to use from pexels.com, by Kai Pilger: kaipilger.myportfolio.com-->
-		<center>
-			<br/><h2>Welcome to Matheroids!</h2><br/>
-			<form action="student_login.php" method="post" id="form_id">
-				<input type="submit" class="studentlogin" value="STUDENT LOGIN">
-			</form>
-			<br><br><br><br>
-			<form action="teacher_login.php" method="post" id="form_id">
-				<input type="submit" class="teacherlogin" value="TEACHER LOGIN">
-			</form>
-			<br>
-			<form action="new_teacher_form.php" method="post" id="form_id">
-				<input type="submit" class="createteacher" value="CREATE TEACHER ACCOUNT">
-			</form>
-
-		</center>
+	<center>
+		<br/><h2>Welcome to Matheroids!</h2><br/>
+		<form action="student_login.php" method="post" id="form_id">
+			<input type="submit" class="studentlogin" value="STUDENT LOGIN">
+		</form>
+		<br><br><br><br>
+		<form action="teacher_login.php" method="post" id="form_id">
+			<input type="submit" class="teacherlogin" value="TEACHER LOGIN">
+		</form>
+		<br>
+		<form action="new_teacher_form.php" method="post" id="form_id">
+			<input type="submit" class="createteacher" value="CREATE TEACHER ACCOUNT">
+		</form>
+	</center>
 </body>
 
 </html>
