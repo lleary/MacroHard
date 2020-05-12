80d2be869cafa91aa74025842230176ee19209eb
apc518 <56745633+apc518@users.noreply.github.com>
2020-05-11 22:06:45 -0700

    more safeguards for teacher account creation
    the input fields must not contain spaces, all fields must be filled, and the firstname/lastname must not already be in the database

--- Project Folder/create_teacher.php
+++ Project Folder/create_teacher.php
@@ -10,6 +10,20 @@
 	echo "Teacher ".$firstname." ".$lastname." added";
 
 	function createTeacherAccount($firstname, $lastname, $password, $cPassword, $class){
+		// must not be an account that already exists
+		$all_users = get_user_info(USERFILE);
+		foreach($all_users as $user){
+			if($user["first"] === $firstname || $user["last"] === $lastname){
+				inputError();
+			}
+		}
+
+		// inputs must not contain spaces
+		if(strpos($firstname, ' ') !== false){ inputError(); }
+		if(strpos($lastname, ' ') !== false){ inputError(); }
+		if(strpos($password, ' ') !== false){ inputError(); } // no need to check for $cPassword since it must be equal to password
+		if(strpos($class, ' ') !== false){ inputError(); }
+
 		if($cPassword === $password && $firstname != '' && $lastname != '' && $password != '' && $class != ''){
 			$teacher_info = array($firstname, $lastname, "teacher", password_hash($password, PASSWORD_DEFAULT), "4", $class, "0", "0", "0", "0");
 
@@ -18,7 +32,13 @@
 			header("Location: teacher_login.php");
 		}
 		else{
-			header("Location: new_teacher_form.php");
+			inputError();
 		}
+	}
+
+	function inputError(){
+		// we should add something to this function to inform the user what went wrong
+		header("Location: new_teacher_form.php");
+		exit;
 	}
 ?>
