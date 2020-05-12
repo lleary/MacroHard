e74c57a1d1f27983dc4608f0be553d511ce38577
apc518 <56745633+apc518@users.noreply.github.com>
2020-05-11 19:27:48 -0700

    fixed up teacher account creation
    the page now asks you to input the password twice to confirm it. If the passwords dont match the form is refreshed.
    
    On successful account creation, the page redirects to teacher login rather than the welcome page
    
    The passwords also get hashed on creation

--- Project Folder/create_teacher.php
+++ Project Folder/create_teacher.php
@@ -6,14 +6,19 @@
 
 	extract($_POST);
 
-	createTeacherAccount($firstname, $lastname, $password, $class);
+	createTeacherAccount($firstname, $lastname, $password, $cPassword, $class);
 	echo "Teacher ".$firstname." ".$lastname." added";
 
-	function createTeacherAccount($firstname, $lastname, $password, $class){
-		$teacher_info = array($firstname, $lastname, "teacher", $password, "4", $class, "0", "0", "0", "0");
+	function createTeacherAccount($firstname, $lastname, $password, $cPassword, $class){
+		if($cPassword === $password){
+			$teacher_info = array($firstname, $lastname, "teacher", password_hash($password, PASSWORD_DEFAULT), "4", $class, "0", "0", "0", "0");
 
-		save_data(USERFILE, $teacher_info);
+			save_data(USERFILE, $teacher_info);
+
+			header("Location: teacher_login.php");
+		}
+		else{
+			header("Location: new_teacher_form.php");
+		}
 	}
-
-	header("Location: welcome.php");
 ?>
