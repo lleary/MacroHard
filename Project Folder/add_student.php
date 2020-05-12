0fb35ebafa8abff0714d5476e631507d1955ae4a
apc518 <56745633+apc518@users.noreply.github.com>
2020-05-11 18:52:28 -0700

    made some variable names right
    creating student was using incorrect variable names

--- Project Folder/add_student.php
+++ Project Folder/add_student.php
@@ -6,11 +6,11 @@
 
 	extract($_POST);
 
-	createStudentAccount($username, $password);
-	echo "Student ".$username." ".$password." added";
+	createStudentAccount($firstname, $lastname);
+	echo "Student ".$firstname." ".$lastname." added";
 
-	function createStudentAccount($username, $password){
-		$student_info = array($username, $password, "student", "0", "1", "0", "0", "0", "0", "0");
+	function createStudentAccount($firstname, $lastname){
+		$student_info = array($firstname, $lastname, "student", "0", "1", "0", "0", "0", "0", "0");
 
 		save_data(USERFILE, $student_info);
 	}
